5c1545ad2ec53e31f1f210af69c5a212b9e95b36
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2024-07-20 20:36:19 +0100

    Initial Commit

---
+++ AnimationPlayground/AnimationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPlaygroundApp.swift
+//  AnimationPlayground
+//
+//  Created by Kod Enigma on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
